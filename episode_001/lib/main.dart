bf526b68e0c8dd3c77c274419017ca44dfeb9632
XavierChanth <xchanthavong@gmail.com>
2021-10-22 14:10:06 -0400

    Update chit_chat to latest dependencies

--- episode_001/lib/main.dart
+++ episode_001/lib/main.dart
@@ -26,7 +26,6 @@ Future<AtClientPreference> loadAtClientPreference() async {
         ..hiveStoragePath = dir.path
         ..commitLogPath = dir.path
         ..isLocalStoreRequired = true
-        ..syncStrategy = SyncStrategy.IMMEDIATE
       // TODO set the rest of your AtClientPreference here
       ;
 }
@@ -41,10 +40,7 @@ class _MyAppState extends State<MyApp> {
   // * load the AtClientPreference in the background
   Future<AtClientPreference> futurePreference = loadAtClientPreference();
 
-  AtClientService? atClientService;
   AtClientPreference? atClientPreference;
-
-  Map<String, bool> _clientIsInitialized = {};
 
   final AtSignLogger _logger = AtSignLogger(AtEnv.appNamespace);
 
@@ -69,29 +65,15 @@ class _MyAppState extends State<MyApp> {
                     context: context,
                     atClientPreference: atClientPreference!,
                     domain: AtEnv.rootDomain,
+                    rootEnvironment: AtEnv.rootEnvironment,
                     onboard: (value, atsign) {
-                      setState(() {
-                        atClientService = value[atsign]!;
-                      });
-                      var instance = atClientService!.atClient!;
-                      initializeContactsService(
-                        instance,
+                      initializeContactsService(rootDomain: AtEnv.rootDomain);
+                      initializeChatService(
+                        AtClientManager.getInstance(),
                         atsign!,
                         rootDomain: AtEnv.rootDomain,
                       );
 
-                      if (_clientIsInitialized[atsign] ?? false) {
-                        var chatService = ChatService();
-                        chatService.currentAtSign = atsign;
-                        chatService.atClientInstance = instance;
-                      } else {
-                        initializeChatService(
-                          instance,
-                          atsign,
-                          rootDomain: AtEnv.rootDomain,
-                        );
-                        _clientIsInitialized[atsign] = true;
-                      }
                       _logger.finer('Successfully onboarded $atsign');
                     },
                     onError: (error) {
@@ -104,51 +86,6 @@ class _MyAppState extends State<MyApp> {
               ],
             ),
           ),
-        ),
-      ),
-
-      // * The context provider for the app
-      builder: (BuildContext context, Widget? child) {
-        if (atClientService != null && atClientPreference != null) {
-          return AtContext(
-            atClientService: atClientService!,
-            atClientPreference: atClientPreference!,
-            child: child ?? Container(),
-          );
-        }
-        return child ?? Container();
-      },
-    );
-  }
-}
-
-//* The next screen after onboarding (second screen)
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // * Get the AtContext from build context
-    // ! NOTE: Only use this after successfully onboarding the @sign
-    AtContext atContext = AtContext.of(context);
-
-    // * Example Uses
-    /// AtClientService atClientService = atContext.atClientService;
-    /// AtClientImpl? atClientInstance = atContext.atClient;
-    /// String? currentAtSign = atContext.currentAtSign;
-    /// AtClientPreference atClientPreference = atContext.atClientPreference;
-    /// atContext.switchAtsign("@example");
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text(
-                'Successfully onboarded and navigated to FirstAppScreen'),
-            Text('Current @sign: ${atContext.currentAtSign}'),
-          ],
         ),
       ),
     );
